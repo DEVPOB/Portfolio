e7952df03255aa21bbf84c0e4d89a96c87ec0b00
DEVPOB <86759611+DEVPOB@users.noreply.github.com>
2022-03-25 20:14:42 +0700

    commit

--- JS/Main.js
+++ JS/Main.js
@@ -1,1 +1,26 @@
-console.log("TEST");
+let ClickTime = -1;
+let something = document.getElementsByClassName("Home_Image");
+function Click(){
+    ClickTime++;  
+    console.log(ClickTime)
+    switch (ClickTime) {
+        case 3:
+            alert("Noting.")
+            break;
+        case 4:
+            alert("Noting.")
+            break;
+        case 5:
+            alert("Noting Here.")
+            break;
+        case 6:
+            alert("Bro Don't Click Noting Here.")
+            break;
+        case 7:
+            alert("If you click one more time I will close this website.")
+            break;
+        case 8:
+            window.close();
+            break;
+    }
+}
